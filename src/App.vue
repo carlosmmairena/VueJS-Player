081afdf230373a6e0f3c36fe8bd7fb0a91155fe3
Carlos Mairena <carlosmiguelmairena@gmail.com>
2021-10-18 00:21:58 -0600

    Preview of the custom player :)

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,10 @@
     <v-main>
       <v-container 
         class="d-flex align-center mx-auto fill-height" 
-        :style="{ width: '70%' }"
-        >
-        <!-- <Player /> -->
-        <slider :current_value="value" @input="val => (value = val)" />
+        :style="{ width: '60%' }"
+      >
+        <Player />
+        <!-- <slider :current_value="value" @input="val => (value = val)" /> -->
       </v-container>
     </v-main>
 
@@ -16,15 +16,13 @@
 
 <script>
 
-import Slider from './components/Slider/Slider.vue';
-//import Player from './components/Player';
+import Player from './components/Player';
 
 export default {
   name: "App",
 
   components: {
-    //Player,
-    Slider,
+    Player,
   },
 
   data: () => ({
--- src/components/Player/Player.vue
+++ src/components/Player/Player.vue
@@ -1,18 +1,59 @@
 <template>
-    <v-container class="player">
-        <v-container class="player__sizer">
-            <video controls>
+    <div class="player">
+        <div class="player__sizer">
+            <video>
                 <source type="video/mp4" src="@/assets/videos/my_video.mp4"/>
             </video>
-        </v-container>
-    </v-container>
+        </div>
+
+        <div class="player__gradient" />
+
+        <div class="player__controls">
+            <v-toolbar dense color="transparent" flat class="player__toolbar">
+                <v-btn color="white" icon>
+                    <v-icon>mdi-play</v-icon>
+                </v-btn>
+
+                <v-btn color="white" icon>
+                    <v-icon>mdi-volume-high</v-icon>
+                </v-btn>
+
+                <Slider 
+                    class="player__sound-slider"
+                    :style="{ width: '96px' }"
+                    :current_value="value"
+                    @input="val => (value = val)"
+                />
+
+                <div class="player__time ml-4">
+                    00:00:00
+                </div>
+
+                <v-spacer />
+
+                <v-btn color="white" icon>
+                    <v-icon>mdi-fullscreen</v-icon>
+                </v-btn>
+            </v-toolbar>
+        </div>
+    </div>
 </template>
 
 <script>
 import "./Player.scss";
+import Slider from "@/components/Slider";
 
 export default {
     name: "Player",
+    
+    components: {
+        Slider
+    },
+
+    data: () => ({
+        value: 1
+    }),
+
 }
 
 </script>
